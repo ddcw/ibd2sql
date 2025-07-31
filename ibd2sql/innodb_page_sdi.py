71ada35613f0d2f3e29b61a5b0ca670ff61cb8d6
ddcw <ddcw@localhost>
2025-07-31 10:04:14 +0800

    issue:70 bit default b''

--- ibd2sql/innodb_page_sdi.py
+++ ibd2sql/innodb_page_sdi.py
@@ -101,8 +101,10 @@ class TABLE(object):
 			if col["default_option"] != "":
 				#ddl += f" DEFAULT ({col['default_option']})"
 				ddl += f" DEFAULT ({col['default_option']})" if self.mysqld_version_id > 80012 else f" DEFAULT {col['default_option']}"
-			else:
-				ddl += f"{' DEFAULT '+repr(col['default']) if col['have_default'] else ''}" #default
+			elif col['have_default']: # issue 70
+				ddl += ' DEFAULT '
+				ddl += col['default'] if col['ct'] in ['bit','binary','varbinary'] else repr(col['default'])
+				#ddl += f"{' DEFAULT '+repr(col['default']) if col['have_default'] else ''}" #default
 			ddl += f"{' AUTO_INCREMENT' if col['is_auto_increment'] else ''}" #auto_increment
 			ddl += f"{' ON UPDATE '+col['update_option'] if 'update_option' in col and col['update_option']!='' else ''}"
 			ddl += f"{' COMMENT '+repr(col['comment']) if col['comment'] != '' else '' }" #comment
